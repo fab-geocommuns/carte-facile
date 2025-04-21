46c915972e2ef8d4bd4e9c8714d7b728b2dc59b0
Romain Ronflette <contact@romainronflette.com>
2025-04-21 18:33:10 +0200

    Remove CommonJS output configuration from Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -8,11 +8,6 @@ export default {
   input: 'src/index.ts',
   output: [
     {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
       file: 'dist/index.esm.js',
       format: 'es',
       sourcemap: true,
