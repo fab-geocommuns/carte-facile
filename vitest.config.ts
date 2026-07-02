b910f43259b8679efce0248e62a6e4599d768cf0
nico <contact@nardu.in>
2026-07-02 12:27:40 +0200

    feat(hackaton-refactor): update CI tests to reflect changes and modernize node test versions

--- vitest.config.ts
+++ vitest.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
 					name: "browser",
 					browser: {
 						enabled: true,
+						headless: true,
 						provider: playwright(),
 						instances: [{ browser: "chromium" }]
 					}
